dc23bd310cdf310f71800b1245348938c0787b58
partyy1211 <partenia.gamil@student.giu-uni.de>
2025-11-11 09:58:02 +0200

    Update Course.h

--- Course.h
+++ Course.h
@@ -4,19 +4,25 @@
 #include <string>
 #include "Student.h"
 using namespace std;
-//#write your code here
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+class course{
+    private:
+    string coursecode;
+    string coursename;
+    student*student;
+    int currentstudent;
+    int maxstudents;
+    public:
+    addstudent(const student& s);
+    dispalyCourseInfo();
+    course(string coursecode,string coursename,int maxstudents,int currentstudent,student*student);
+    ~course();
+     string setcoursecode();
+      string setcoursename();
+       int setcurrentstudent();
+        int setmaxstudents();
+        string getcoursecode();
+      string getcoursename();
+       int getcurrentstudent();
+        int getmaxstudents();
+}
 #endif
